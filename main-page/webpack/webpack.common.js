d81e03ecb4ab3367c6014f95ebbfae50ceba4ba6
Rustam <dzhaksimov.rustam@gmail.com>
2022-08-16 22:24:57 +0300

    feat: start header

--- main-page/webpack/webpack.common.js
+++ main-page/webpack/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
     new CopyPlugin({
-      patterns: [{ from: 'source', to: 'dest' }]
+      patterns: [{ from: 'src', to: 'dest' }]
     })
   ],  
 }
