0fdc4b3b1959686e40df81be79d9e6c58bfa4030
Mitya <partyzan327@gmail.com>
2022-09-04 01:30:03 +0300

    fix: fix game reloading error

--- main-page/src/audiochallenge/index.tsx
+++ main-page/src/audiochallenge/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import { Startscreen } from './startscreen/startscreen'
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<Startscreen group={1} />)
+// import React from 'react'
+// import * as ReactDOM from 'react-dom/client'
+// import { Startscreen } from './startscreen/startscreen'
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+// root.render(<Startscreen group={1} />)
